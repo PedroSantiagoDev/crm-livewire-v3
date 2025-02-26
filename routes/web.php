da041587ab6a1cf0a682c3980a47bd25b709a594
Pedro Santiago <joaopedrosantiago1103@gmail.com>
2025-02-26 13:25:05 -0300

    be able to success log in

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,6 @@ use App\Livewire\Auth\Register;
 use App\Livewire\Welcome;
 use Illuminate\Support\Facades\{Auth, Route};
 
-Route::get('/', Welcome::class)->name('welcome');
+Route::get('/', Welcome::class)->name('dashboard');
 Route::get('/register', Register::class)->name('auth.register');
 Route::get('/logout', fn () => Auth::logout())->name('auth.logout');
-
-Route::get('/dashboard', fn () => 'dashboard')->name('dashboard');
