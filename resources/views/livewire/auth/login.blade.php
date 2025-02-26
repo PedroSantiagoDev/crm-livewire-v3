953ace61d5c7af7b4f60218a74246972c4568b95
Pedro Santiago <joaopedrosantiago1103@gmail.com>
2025-02-26 14:02:21 -0300

    showing error message when attempting to login

--- resources/views/livewire/auth/login.blade.php
+++ resources/views/livewire/auth/login.blade.php
@@ -1,3 +1,5 @@
 <div>
-    {{-- If your happiness depends on money, you will never be happy with yourself. --}}
+    @error('invalidCredentials')
+        <span>{{ $message }}</span>
+    @enderror
 </div>
