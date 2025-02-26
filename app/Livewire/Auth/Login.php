d10953e89326e0a3ee83a41d13e214fb31368cf0
Pedro Santiago <joaopedrosantiago1103@gmail.com>
2025-02-26 15:40:42 -0300

    ensuring login to be secured by rate limiting

--- app/Livewire/Auth/Login.php
+++ app/Livewire/Auth/Login.php
@@ -2,7 +2,8 @@
 
 namespace App\Livewire\Auth;
 
-use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\{Auth, RateLimiter};
+use Illuminate\Support\Str;
 use Illuminate\View\View;
 use Livewire\Component;
 
@@ -14,13 +15,38 @@ class Login extends Component
 
     public function tryToLogin(): void
     {
+
+        if ($this->ensureIsNotRateLimiting()) {
+            return;
+        }
+
         if (!Auth::attempt(['email' => $this->email, 'password' => $this->password])) {
+            RateLimiter::hit($this->throttleKey());
+
             $this->addError('invalidCredentials', trans('auth.failed'));
 
             return;
         }
 
         $this->redirect(route('dashboard'));
+    }
+
+    private function throttleKey(): string
+    {
+        return Str::transliterate(strtolower($this->email) . '|' . request()->ip());
+    }
+
+    private function ensureIsNotRateLimiting(): bool
+    {
+        if (RateLimiter::tooManyAttempts($this->throttleKey(), 5)) {
+            $this->addError('rateLimiter', trans('auth.throttle', [
+                'seconds' => RateLimiter::availableIn($this->throttleKey()),
+            ]));
+
+            return true;
+        }
+
+        return false;
     }
 
     public function render(): View
