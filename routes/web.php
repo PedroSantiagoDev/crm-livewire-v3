f75bf0163cc8cb50199532dff9e9a2c52cb9dd3c
Pedro Santiago <joaopedrosantiago1103@gmail.com>
2025-02-27 16:55:12 -0300

    should be able to logout of the application

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,10 @@
 
 use App\Livewire\Auth\{Login, Register};
 use App\Livewire\Welcome;
-use Illuminate\Support\Facades\{Auth, Route};
+use Illuminate\Support\Facades\{Route};
 
 Route::get('/login', Login::class)->name('login');
 Route::get('/register', Register::class)->name('auth.register');
-Route::get('/logout', fn () => Auth::logout());
 
 Route::middleware('auth')->group(function () {
     Route::get('/', Welcome::class)->name('dashboard');
