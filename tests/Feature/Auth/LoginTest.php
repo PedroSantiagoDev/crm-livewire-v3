803362fa3edac693bb1b2f4ac65af1dfc918205c
Pedro Santiago <joaopedrosantiago1103@gmail.com>
2025-02-27 15:31:54 -0300

    be able to logout of the application

--- tests/Feature/Auth/LoginTest.php
+++ tests/Feature/Auth/LoginTest.php
@@ -36,8 +36,6 @@ it('should make sure to inform the user an error when email and password doesnt 
 });
 
 it('should make sure that the rate limiting is blocking after 5 attempts', function () {
-    $user = User::factory()->create();
-
     for ($i = 0; $i < 5; $i++) {
         Livewire::test(Login::class)
             ->set('email', 'joe@doe.com')
